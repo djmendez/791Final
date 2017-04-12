b8873304813cc31291ef9c86728f187a010d9f38
Niki Silveria <nsilveria@nevada.unr.edu>
2017-04-12 10:17:44 -0700

    converted to include MSN
    Untested still, timestamps need to be discussed

--- GetAction.m
+++ GetAction.m
@@ -1,19 +1,18 @@
-function MSN = GetAction(MSN, Q)
+function MSN = GetAction(MSN, Q, t, p)
     %chooses an action for each robot based on the epsilon greedy policy
     %may want to pass this in later to update value as episodes change
     epsilon = .5;
     
     %for each robot node in MSN
-    for i = 1:size(MSN.node)
+    for i = 1:p.maxnodes
         %get a random number between 0 and 1
         r = rand;
         if (r < epsilon)
-            %if r is less than epsilon choose a random action
-            choices(i) = actions((randi(numel(actions))));
+            %if r is less than epsilon choose a random action for time t
+            MSN.action(t,i) = randi(p.num_actions);
         else
             % else get the max / most optimal action in the current state
-            [~, choices(i)] = max(Q(states(i),:,i));
+            [~, MSN.action(t,i)] = max(Q(MSN.state(t,i)),:,i);
         end
-        ii = ii + 1;
     end
 end
--- QUpdate.m
+++ QUpdate.m
@@ -1,19 +1,23 @@
-function Q = QUpdate(MSN, Q, p)
+function Q = QUpdate(MSN, Q, t, p)
     % performs a q-update step using actions, states, and rewards
     % for each robot's Q matrix update current state action pair
-    for n = 1:num_robots
+    for i = 1:p.maxnodes
         % variables for clarity
-        [~, max_next_action] = max(Q_mat(next_states(n), :, n));
-        Q_state_action = Q_mat(states(n), chosen_actions(n), n);
+        [~, max_next_action] = max(Q(MSN.state(t, i), :, i));
+        Q_state_action = Q_mat(MSN.state(t-1, i), MSN.action(t, i), i);
         
-        update = Q_state_action + (learning_rate * (rewards(n) + ...
-            (discount_factor * max_next_action) - Q_state_action));
+        %do the q update equation
+        update = Q_state_action + (p.learning_rate * (MSN.reward(t,i) + ...
+            (p.discount_factor * max_next_action) - Q_state_action));
         
         % if the trust weight is less than 1, consider neighbors q-values
-        if (trust_weight < 1)
-            update = (trust_weight * update) + ((1 - trust_weight) * sum_neighbors(states(n), chosen_actions(n), Q_mat, neighbors{n}));
-        end
+        % come back to this later
+        %if (trust_weight < 1)
+        %    update = (trust_weight * update) + ((1 - trust_weight) * sum_neighbors(states(i), chosen_actions(i), Q_mat, neighbors{i}));
+        %end
         
-        Q_mat(states(n), chosen_actions(n), n) = update;
+        %state an action should really be a pair at the same times
+        %still need to figure this out
+        Q(MSN.state(t-1, i), MSN.action(t, i), i) = update;
     end
 end
